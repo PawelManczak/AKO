f0f33b2c23985e8ca736327cb3f2b0092ced31db
Paweł Mańczak <pawel14522@gmail.com>
2022-10-23 14:59:16 +0200

    exercise 3

---
+++ zadanie3.asm
@@ -0,0 +1,28 @@
+; Przyk³ad wywo³ywania funkcji MessageBoxA i MessageBoxW
+.686
+.model flat
+extern _ExitProcess@4 : PROC
+extern _MessageBoxA@16 : PROC
+extern _MessageBoxW@16 : PROC
+public _main
+.data
+tytul_Unicode dw 'Z','n','a','k','i', 0
+tekst_Unicode dw 'T','o',' ','j','e','s','t',' ','p','i','e','s'
+dw 0D83DH, 0DC15h
+dw ' ', 'i',' ','k','o','t', ' '
+dw 0D83Dh, 0dc08h
+
+.code
+_main PROC
+
+ push 0 ; stala MB_OK
+; adres obszaru zawieraj¹cego tytu³
+ push OFFSET tytul_Unicode
+; adres obszaru zawieraj¹cego tekst
+ push OFFSET tekst_Unicode
+ push 0 ; NULL
+ call _MessageBoxW@16
+ push 0 ; kod powrotu programu
+ call _ExitProcess@4
+_main ENDP
+END
