af9f2d6391325bf20a8307caa448628b704d61a7
Paweł Mańczak <pawel14522@gmail.com>
2022-11-06 20:16:20 +0100

    spaces without polish chars

---
+++ spaces.asm
@@ -0,0 +1,98 @@
+.686
+.model flat
+extern _ExitProcess@4 : PROC
+extern _MessageBoxA@16 : PROC
+extern __write : PROC ; (dwa znaki podkreślenia)
+extern __read : PROC ; (dwa znaki podkreślenia)
+public _main
+.data
+tytul_Unicode dw 'T','e','k','s','t',' ','w',' '
+dw 'f','o','r','m','a','c','i','e',' '
+dw 'U','T','F','-','1','6', 0
+tekst_Unicode dw 'K','a', 017CH ,'d','y',' ','z','n','a','k',' ' ; dziala utf - 16
+dw 'z','a','j','m','u','j','e',' '
+dw '1','6',' ','b','i', 't',00F3H ,'w', 0
+tytul_Win1250 db 'Tekst w standard123123zie Windows 1250', 0
+tekst_Win1250 db 'Ka', 191, 'dy znak zajmuje 8 bit', 241 ,'w', 0 ; tutaj windows 1250
+
+	tekst_pocz db "wpisz"
+	koniec_t db ?
+	magazyn db 80 dup (?)
+	nowy db 80 dup (?)
+	nowa_linia db 10
+	liczba_znakow dd ?
+	ostatni_znak db ?
+
+.code
+_main PROC
+	 mov ecx,(OFFSET koniec_t) - (OFFSET tekst_pocz)
+	 push ecx
+	 push OFFSET tekst_pocz ; adres tekstu
+	 push 1 ; nr urządzenia (tu: ekran - nr 1)
+	 call __write ; wyświetlenie tekstu początkowego
+	 add esp, 12 ; usuniecie parametrów ze stosu
+
+	 
+
+	 push 80 ; maksymalna liczba znaków
+	 push OFFSET magazyn
+	 push 0 ; nr urządzenia (tu: klawiatura - nr 0)
+	 call __read ; czytanie znaków z klawiatury
+	 add esp, 12 ; usuniecie parametrów ze stosu
+	 mov liczba_znakow, eax
+
+	 mov eax, 0 ; pointer without rejected chars
+	 mov ebx, 0 ; pointer with everythink
+ptl:
+	mov dl, magazyn[eax]
+	
+	cmp eax, 1
+	jb p_dalej
+
+	cmp ostatni_znak, "\"
+	jne p_dalej
+
+
+	cmp dl, "t"
+	jne a
+	mov nowy[ebx-1], " "
+	dec ebx
+	jmp dalej
+
+	a:
+	cmp dl, "a"
+	jne b
+	mov nowy[ebx-1], "ą"
+	dec ebx
+	jmp dalej
+	
+	b:
+
+
+
+
+
+p_dalej:
+	mov nowy[ebx], dl
+dalej:
+	
+	mov ostatni_znak, dl
+	inc eax
+	inc ebx
+
+	cmp eax, liczba_znakow
+	jb ptl
+	
+ push 0 ; stała MB_OK
+; adres obszaru zawierającego tytuł
+ push OFFSET tytul_Win1250 
+; adres obszaru zawierającego tekst
+ push OFFSET nowy
+ push 0 ; NULL
+ call _MessageBoxA@16
+ push 0 ; stala MB_OK
+
+ push 0 ; kod powrotu programu
+ call _ExitProcess@4
+_main ENDP
+END
