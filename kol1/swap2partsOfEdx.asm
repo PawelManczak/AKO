05cea2144bb68fb3c4c480a02fe576ce6f619af8
pawelmanczak <pawelmanczak14@gmail.com>
2022-12-08 18:01:10 +0100

    another option exercise 20

---
+++ kol1/swap2partsOfEdx.asm
@@ -0,0 +1,21 @@
+.686 
+.model flat
+
+public _main
+
+.code
+
+_main PROC
+
+;20. Napisaæ fragment programu w asemblerze, który
+;zamieni m³odsz¹ (bity 15 – 0) i starsz¹ (bity 31 – 16) czêœæ
+;rejestru EDX.
+
+mov edx, 0FFFF0000h
+
+rol edx, 16
+;bswap edx works similar
+
+
+_main ENDP
+END
